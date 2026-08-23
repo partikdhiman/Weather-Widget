b77e7c366d62030d5eb99abbc40cc2c185fcf8c9
Partik Dhiman <dhimanpartik16@gmail.com>
2026-08-24 00:29:07 +0530

    Initial commit: weather app search box

---
+++ src/SearchBox.jsx
@@ -0,0 +1,33 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './SearchBox.css';
+import { useState } from 'react';
+
+export default function SearchBox() {
+    let [city, setCity] = useState("");
+    
+    let handleChange = (event)=>{
+        setCity(event.target.value);
+    }
+
+    let handleSubmit = (event)=>{
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+    }
+
+    return (
+        <>
+            <div className='SearchBox'>
+                <h3>Search for the weather</h3>
+                <form onSubmit={handleSubmit}>
+                    <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
+                    <br /><br />
+                    <Button variant="contained" type='submit' >
+                        Search
+                    </Button>
+                </form>
+            </div>
+        </>
+    )
+}
